f4ff4f9683fc2c25cd9c47cf3ad1d0b343e1f5e0
ronaldo-melo <rpm.melkor@gmail.com>
2020-07-29 20:14:59 -0300

    Create Project

---
+++ src/test/java/com/br/costrucao/CursoSpringRestApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.costrucao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringRestApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
