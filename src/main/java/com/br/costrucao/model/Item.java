72f8e22ebc2e3d63c2f4420d8e94da87c38fcc61
ronaldo-melo <rpm.melkor@gmail.com>
2020-07-30 11:54:35 -0300

    Entidade Item

---
+++ src/main/java/com/br/costrucao/model/Item.java
@@ -0,0 +1,95 @@
+package com.br.costrucao.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private BigDecimal valorDeUnidade;
+	
+	@Column(nullable = false)
+	private String codigo;
+	
+	@Column(nullable = false)
+	private BigDecimal icms;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getValorDeUnidade() {
+		return valorDeUnidade;
+	}
+
+	public void setValorDeUnidade(BigDecimal valorDeUnidade) {
+		this.valorDeUnidade = valorDeUnidade;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public BigDecimal getIcms() {
+		return icms;
+	}
+
+	public void setIcms(BigDecimal icms) {
+		this.icms = icms;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
