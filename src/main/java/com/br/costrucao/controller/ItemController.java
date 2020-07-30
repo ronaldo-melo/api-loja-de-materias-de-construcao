337dafd38be244e880d1d5f3ae8a83167a78fd0e
ronaldo-melo <rpm.melkor@gmail.com>
2020-07-30 14:55:38 -0300

    endpoint Items - remove

--- src/main/java/com/br/costrucao/controller/ItemController.java
+++ src/main/java/com/br/costrucao/controller/ItemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.costrucao.exceptions.EntityNotFoundException;
 import com.br.costrucao.model.Item;
 import com.br.costrucao.repository.ItemRepository;
 import com.br.costrucao.service.ItemService;
@@ -41,6 +43,16 @@ public class ItemController {
 		Item save = service.save(item);
 		/*200 ok é errado deve ser 201 created*/
 		return ResponseEntity.ok(save);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Item> remove(@PathVariable Long id){
+		try {
+			service.deleteById(id);
+			return ResponseEntity.noContent().build();
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }	
 
