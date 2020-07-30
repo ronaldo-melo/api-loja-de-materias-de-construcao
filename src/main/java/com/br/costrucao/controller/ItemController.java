99d2a6b4592d13429196e8763b769b043f242138
ronaldo-melo <rpm.melkor@gmail.com>
2020-07-30 17:40:11 -0300

    Item Controller - update item end-point

--- src/main/java/com/br/costrucao/controller/ItemController.java
+++ src/main/java/com/br/costrucao/controller/ItemController.java
@@ -1,13 +1,16 @@
 package com.br.costrucao.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +53,16 @@ public class ItemController {
 		try {
 			service.deleteById(id);
 			return ResponseEntity.noContent().build();
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable Long id, @PathVariable Item item){
+		try {
+			item = service.update(id, Optional.of(item));
+			return ResponseEntity.status(HttpStatus.OK).body(item);
 		} catch (EntityNotFoundException e) {
 			return ResponseEntity.notFound().build();
 		}
