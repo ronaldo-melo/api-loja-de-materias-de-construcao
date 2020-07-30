96d573768437ed872d6b5324def9286e8e65af36
ronaldo-melo <rpm.melkor@gmail.com>
2020-07-30 18:03:52 -0300

    Using the Functional Interface Supplier<T> interface to improve code readability and make it less extensive

--- src/main/java/com/br/costrucao/service/ItemService.java
+++ src/main/java/com/br/costrucao/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.br.costrucao.service;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,20 +17,20 @@ public class ItemService {
 	@Autowired
 	private ItemRepository repository;
 	
+	Supplier<RuntimeException> supplier = () ->  new EntityNotFoundException("There is no item registred with that id");
+	
 	public Item save(Item entity) {
 		return repository.save(entity);
 	}
 	
 	public void deleteById(Long id) {
-		Item item = repository.findById(id).orElseThrow(
-				() -> new EntityNotFoundException("There is no item registred with that id"));
+		Item item = repository.findById(id).orElseThrow(supplier);
 		repository.deleteById(item.getId());
 	}
 	
 	public Item update(Long id, Optional<Item> item) {
 		
-		Item salvo = repository.findById(id).orElseThrow(
-				() -> new EntityNotFoundException("There is no item registred with that id"));
+		Item salvo = repository.findById(id).orElseThrow(supplier);
 		
 		BeanUtils.copyProperties(item.get(), salvo, "id");
 		return repository.save(salvo);
