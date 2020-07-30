2645dcb11ebda7866b53808fddf0b85b577e6eef
ronaldo-melo <rpm.melkor@gmail.com>
2020-07-30 14:40:47 -0300

    service for remove items

--- src/main/java/com/br/costrucao/service/ItemService.java
+++ src/main/java/com/br/costrucao/service/ItemService.java
@@ -3,6 +3,7 @@ package com.br.costrucao.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.br.costrucao.exceptions.EntityNotFoundException;
 import com.br.costrucao.model.Item;
 import com.br.costrucao.repository.ItemRepository;
 
@@ -14,6 +15,12 @@ public class ItemService {
 	
 	public Item save(Item entity) {
 		return repository.save(entity);
+	}
+	
+	public void deleteById(Long id) {
+		Item item = repository.findById(id).orElseThrow(
+				() -> new EntityNotFoundException("There is no item registred with that name"));
+		repository.deleteById(item.getId());
 	}
 	
 }
